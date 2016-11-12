8eb5791d7c3ddf0e4b38e124de10b5ac52f8b842
DaveBaas <davebaas@outlook.com>
2016-11-12 16:49:57 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment 2 of the coursera class R 
+## programming, student Dave Baas
+## Two functions are created to calculate a matrix' inverse and 
+## cache the inverse. 
 
-## Write a short comment describing this function
+## The function makeCachematrix creates a special matrix object, 
+## in the form of a list, which can cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL                                       ## set m to empty variable
+        set <- function(y) {                            ## make the function set
+                x <<- y                                 ## set y to x in other environment
+                m <<- NULL
+        }
+        get <- function() x                             ## make function to get x 
+        setinv <- function(inv) m <<- inv               ## set inv to m and store in other environment
+        getinv <- function() m                          ## get m 
+        list(set = set, get = get,                      ## return list with set,get,setinv,getinv
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The function cachesolve checks if the inverse availabe in 
+## cache, if so it return the inverse from cache. If not, it 
+## calculates and returns inverse and stores in cache. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()                                 ## get cached data
+        if(!is.null(m)) {                               ## continue if m has a value
+                message("getting the cached data")      ## return message
+                return (m)                              ## return inverse from cache
+        }
+        data <- x$get()                                 ## if not, store matrix in data
+        m <- solve(data,...)                            ## calculate inverse
+        x$setinv(m)                                     ## store inverse in cache
+        m                                               ## return inverse m
 }
